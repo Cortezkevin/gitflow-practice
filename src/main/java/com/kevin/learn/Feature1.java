fb0ddd5c73d7a769a9e740c1d7bb4dcdb758f680
Cortezkevin <A19105415@idat.edu.pe>
2025-09-02 18:19:43 -0500

    [feat] - Feature 1 terminado

---
+++ src/main/java/com/kevin/learn/Feature1.java
@@ -0,0 +1,15 @@
+package com.kevin.learn;
+
+public class Feature1 {
+    public static void main(String[] args) {
+        // TODO: THIS IS A FEATURE 1
+    }
+
+    public void cambio2(){
+        return;
+    }
+
+    public void cambio3(){
+        return;
+    }
+}
